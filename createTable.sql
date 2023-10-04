ae830f5dfc58377c1c616580a83d4593cc3a4931
Aleksandr <aleksandr.koshkin.2000@mail.ru>
2023-10-04 13:23:52 +0300

    Two

--- createTable.sql
+++ createTable.sql
@@ -1,9 +1,9 @@
-create schema test
+create schema test;
 create table persons(
-name varchar(255)
-surname varchar(255)
-age int
-phone_number int
-city_of_living  varchar(255)
+name varchar(255),
+surname varchar(255),
+age int,
+phone_number int,
+city_of_living  varchar(255),
 primary key (name, surname, age)
 );
